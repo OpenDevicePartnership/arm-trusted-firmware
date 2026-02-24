c693c475f6850af29f1550aee9e9e9986222c7b3
Phil Weber <philweber@microsoft.com>
2026-02-24 15:09:50 -0800

    Updated we don't need to force BL31_RESET and moved to uuid only for BL32

--- plat/qemu/qemu_sbsa/platform.mk
+++ plat/qemu/qemu_sbsa/platform.mk
@@ -40,11 +40,10 @@ ifeq ($(add-lib-optee),yes)
 BL2_SOURCES		+=	lib/optee/optee_utils.c
 endif
 
-BL2_SOURCES		+=	common/uuid.c
-RESET_TO_BL31		:=	1
 
 ifeq ($(NEED_BL32),yes)
 $(eval $(call add_define,QEMU_LOAD_BL32))
+BL2_SOURCES		+=	common/uuid.c
 endif
 
 # Include GICv3 driver files
